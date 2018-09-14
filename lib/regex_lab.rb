53cfb4912d95cdf0462f9f35c20db01c83df5a6a
Erin Green <erin.y.green@gmail.com>
2018-09-14 01:04:26 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,9 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/\b+un\w+ing/)
+if text.scan(/\b+un\w+ing/)
+  return true
+end
 end
 
 def words_five_letters_long(text)
